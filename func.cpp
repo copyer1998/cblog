a5bd2169ea8da42a39a199923a67f0e13055371e
copyer1998 <2782644531@qq.com>
2019-03-31 15:37:10 +0800

    add

--- func.cpp
+++ func.cpp
@@ -6,7 +6,13 @@
 #include <iostream>
 
 using namespace std;
-
+const int  MAZ=3;
 int main() {
-    cout << "this.te";
+    int var[MAZ]={10,34,300};
+    int *ptr;
+    ptr=var;
+    for (int i = 0; i < 3; ++i) {
+        cout << "var ["<< i <<"]address";
+
+    }
 }
