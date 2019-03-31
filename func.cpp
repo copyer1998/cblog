a4afc2ca75278874c8acacd311536c851607b086
copyer1998 <2782644531@qq.com>
2019-03-31 15:29:38 +0800

    add

---
+++ func.cpp
@@ -0,0 +1,12 @@
+//
+// Created by HFUT-SOM on 2019/3/31.
+//
+
+#include "func.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "this.te";
+}
---
+++ func.h
@@ -0,0 +1,8 @@
+//
+// Created by HFUT-SOM on 2019/3/31.
+//
+
+#ifndef CBLOG_FUNC_H
+#define CBLOG_FUNC_H
+
+#endif //CBLOG_FUNC_H
